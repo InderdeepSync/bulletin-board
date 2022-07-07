c3bc095117cdae86722800e7ccf6602ef62a1151
InderdeepSync <buntycool1998@gmail.com>
2022-07-07 23:11:51 +0530

    Refactoring And Checkpoint

--- board-server.cpp
+++ board-server.cpp
@@ -12,6 +12,7 @@
 #include <set>
 #include <functional>
 #include <algorithm>
+#include <cassert>
 
 #include "board-server.h"
 #include "tcp-utils.h"
@@ -181,6 +182,98 @@ void readMessageFromFile(int messageNumberToRead, int socketToRespond) {
     }
 }
 
+int obtainLengthOfLineToBeReplaced(int messageNumberToReplace, int &totalBytesBeforeLineToReplace) {
+    int fileDescriptor = open(bulletin_board_file,  O_RDONLY,
+                              S_IRGRP | S_IROTH | S_IRUSR | S_IWUSR | S_IWGRP | S_IWOTH);
+
+    const int ALEN = 256;
+    int n, l = 0; char textLine[ALEN];
+
+    while ((n = readline(fileDescriptor, textLine, ALEN - 1)) != recv_nodata) {
+        if (l == messageNumberToReplace) {
+            close(fileDescriptor);
+            return n + 1;
+        } else if (l < messageNumberToReplace) {
+            totalBytesBeforeLineToReplace += (n + 1);
+        }
+        l++;
+    }
+    assert(false); // Will Never happen anyway.
+}
+
+void optimalReplaceAlgorithm(string newUser, int messageNumberToReplace, string new_message) {
+    char lineToStore[4096];
+    memset(lineToStore, 0, sizeof lineToStore);
+    snprintf(lineToStore, 4096, "%d/%s/%s\n", messageNumberToReplace, newUser.c_str(), new_message.c_str());
+
+    int totalBytesBeforeLineToReplace = 0;
+    int lengthOfLineToBeReplaced = obtainLengthOfLineToBeReplaced(messageNumberToReplace, totalBytesBeforeLineToReplace);
+    if (lengthOfLineToBeReplaced == strlen(lineToStore)) {
+        // Case 1: Old and New line are of same length
+        int fd3 = open(bulletin_board_file,  O_WRONLY,
+                       S_IRGRP | S_IROTH | S_IRUSR | S_IWUSR | S_IWGRP | S_IWOTH);
+
+        lseek(fd3, totalBytesBeforeLineToReplace, SEEK_SET);
+        write(fd3, lineToStore, strlen(lineToStore));
+
+        close(fd3);
+    } else {
+        int differenceInLength = strlen(lineToStore) - lengthOfLineToBeReplaced;
+
+        int fd1 = open(bulletin_board_file, O_RDONLY,
+                       S_IRGRP | S_IROTH | S_IRUSR | S_IWUSR | S_IWGRP | S_IWOTH);
+        int fd2 = open(bulletin_board_file, O_WRONLY,
+                       S_IRGRP | S_IROTH | S_IRUSR | S_IWUSR | S_IWGRP | S_IWOTH);
+
+        if (differenceInLength < 0) {
+            // Case 2: Shift Backwards and Truncate
+            lseek(fd1, totalBytesBeforeLineToReplace + lengthOfLineToBeReplaced, SEEK_SET);
+
+            lseek(fd2, totalBytesBeforeLineToReplace, SEEK_SET);
+            write(fd2, lineToStore, strlen(lineToStore));
+            long currentWriterPosition = lseek(fd2, 0, SEEK_CUR);
+
+            struct stat fileStatistics;
+            fstat(fd2, &fileStatistics);
+
+            char readOutput[256];
+            int fileSizeAfterReplacement = fileStatistics.st_size + differenceInLength;
+
+            while (currentWriterPosition < fileSizeAfterReplacement) {
+                memset(readOutput, 0, sizeof readOutput);
+                read(fd1, readOutput, 1);
+                write(fd2, readOutput, 1);
+
+                currentWriterPosition = lseek(fd2, 0, SEEK_CUR);
+            }
+
+            ftruncate(fd2, fileSizeAfterReplacement);
+        } else {
+            // Case 3: Shift Forward
+            long currentReaderPosition = lseek(fd1, 0 - 1, SEEK_END);
+            lseek(fd2, differenceInLength - 1, SEEK_END);
+
+            char readOutput[256];
+
+            int temp = totalBytesBeforeLineToReplace + lengthOfLineToBeReplaced;
+            while (currentReaderPosition >= temp) {
+                memset(readOutput, 0, sizeof readOutput);
+                read(fd1, readOutput, 1);
+                write(fd2, readOutput, 1);
+
+                currentReaderPosition = lseek(fd1, -2, SEEK_CUR);
+                lseek(fd2, -2, SEEK_CUR);
+            }
+
+            lseek(fd2, totalBytesBeforeLineToReplace, SEEK_SET);
+            write(fd2, lineToStore, strlen(lineToStore));
+        }
+
+        close(fd1);
+        close(fd2);
+    }
+}
+
 void replaceMessageInFile(const string& user, const string& messageNumberAndMessage, int socketToSend) {
     vector<string> replaceArguments;
     tokenize(messageNumberAndMessage, "/", replaceArguments);
@@ -213,35 +306,7 @@ void replaceMessageInFile(const string& user, const string& messageNumberAndMess
             sleep(6);
         }
 
-        int fd = open(bulletin_board_file, O_RDONLY,
-                      S_IRGRP | S_IROTH | S_IRUSR | S_IWUSR | S_IWGRP | S_IWOTH);
-        int fd2 = open("tempfile", O_WRONLY | O_CREAT,
-                       S_IRGRP | S_IROTH | S_IRUSR | S_IWUSR | S_IWGRP | S_IWOTH);
-
-        const int ALEN = 256;
-        int l = 0; char textLine[ALEN];
-
-        while (readline(fd, textLine, ALEN - 1) != recv_nodata) {
-            vector<string> messageTokens;
-            tokenize(string(textLine), "/", messageTokens);
-
-            if (l == messageNumberToReplace) {
-                char lineToStore[4096];
-                memset(lineToStore, 0, sizeof lineToStore);
-                snprintf(lineToStore, 4096, "%s/%s/%s", messageTokens[0].c_str(), user.c_str(), new_message.c_str());
-
-                write(fd2, lineToStore, strlen(lineToStore));
-            } else {
-                write(fd2, textLine, strlen(textLine));
-            }
-            write(fd2, "\n", 1);
-            l++;
-        }
-
-        close(fd);
-        unlink(bulletin_board_file);
-        close(fd2);
-        rename("tempfile", bulletin_board_file);
+        optimalReplaceAlgorithm(user, messageNumberToReplace, new_message);
 
         if (delayOperations) {
             cout << "Exiting Critical Region. WRITE operation by thread - " << threadId << " completed!"
@@ -313,8 +378,7 @@ void handle_bulletin_board_client(int master_socket) {
             } else if (inputCommand.rfind("WRITE", 0) == 0) {
                 writeToFile(user, tokens[1], slave_socket);
             } else if (inputCommand.rfind("READ", 0) == 0) {
-                const int messageNumberToRead = stoi(tokens[1].c_str());
-                readMessageFromFile(messageNumberToRead, slave_socket);
+                readMessageFromFile(stoi(tokens[1].c_str()), slave_socket);
             } else if (inputCommand.rfind("REPLACE", 0) == 0) {
                 replaceMessageInFile(user, tokens[1], slave_socket);
             } else {
@@ -374,8 +438,6 @@ int board_server(char **argv) {
         }
     }
 
-    cout << "Process ID: " << getpid() << endl;
-
     signal(SIGHUP, sighup_handler); // kill -HUP <Process ID>
 //    signal(SIGINT, sigquit_handler); // kill -INT <Process ID> or Ctrl + C
     signal(SIGQUIT, sigquit_handler); // kill -QUIT <Process ID> or Ctrl + \ [Does not work on CLion for some reason]
@@ -386,8 +448,8 @@ int board_server(char **argv) {
     return 0;
 }
 
-//int main(int argc, char **argv, char *envp[]) {
-//    board_server(argv);
-//}
+int main(int argc, char **argv, char *envp[]) {
+    board_server(argv);
+}
 
 
