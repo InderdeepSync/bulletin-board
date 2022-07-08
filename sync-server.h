e7a1fe896bec270d9856adb30caaee3fce5d97e8
InderdeepSync <buntycool1998@gmail.com>
2022-07-09 04:22:33 +0530

    Add sync-server.h

---
+++ sync-server.h
@@ -0,0 +1,9 @@
+
+#ifndef BULLETIN_BOARD_SYNC_SERVER_H
+#define BULLETIN_BOARD_SYNC_SERVER_H
+
+void sync_server_sigquit_handler(int signum);
+
+void sync_server_sighup_handler(int signum);
+
+#endif //BULLETIN_BOARD_SYNC_SERVER_H
