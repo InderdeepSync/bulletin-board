df3d4a56a39f9bf69623b5c379b935a14082b83c
InderdeepSync <buntycool1998@gmail.com>
2022-07-20 00:39:27 +0530

    Allow ignoring out parameters passed by reference

--- logger.cpp
+++ logger.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <unistd.h>
+#include <stdarg.h>
+
 #include "logger.h"
 
 bool shouldDelayOperations;
@@ -19,4 +21,14 @@ void logDebugMessage(char* message, int sleepDurationInSeconds) {
             sleep(sleepDurationInSeconds);
         }
     }
+}
+
+void debug_printf(const char *format, ...) {
+    va_list argptr;
+    va_start(argptr, format);
+
+    if (shouldDelayOperations)
+        vprintf(format, argptr);
+
+    va_end(argptr);
 }
--- logger.h
+++ logger.h
@@ -6,6 +6,8 @@ void setDebuggingPreference(bool isDelayEnabled);
 
 bool getDebuggingPreference();
 
-void logDebugMessage(char* message, int sleepDurationInSeconds);
+void logDebugMessage(char* message, int sleepDurationInSeconds = 0);
+
+void debug_printf(const char *format, ...);
 
 #endif //BULLETIN_BOARD_LOGGER_H
