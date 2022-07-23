a22ec4962dfe922a2189476ae1bcd950374d70a8
InderdeepSync <buntycool1998@gmail.com>
2022-07-23 07:57:00 +0530

    Simplify Startup

--- board-server.h
+++ board-server.h
@@ -2,7 +2,7 @@
 #ifndef BULLETIN_BOARD_BOARD_SERVER_H
 #define BULLETIN_BOARD_BOARD_SERVER_H
 
-int board_server(char **argv);
+int board_server(int port, int maxThreads, std::vector<std::string> peers);
 
 void reconfigureGlobalVariablesAndRestartBoardServer(std::string configurationFile);
 
